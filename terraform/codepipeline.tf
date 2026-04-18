a13aa743d0e4f66b9a9398aa7b4cba6bd91935fd
jatindeshpande <jatinvd1997@gmail.com>
2026-04-18 18:31:09 +0530

    update

--- terraform/codepipeline.tf
+++ terraform/codepipeline.tf
@@ -1,134 +1,134 @@
-resource "aws_codepipeline" "codepipeline" {
-  name     = "gluejob-pipeline"
-  role_arn = aws_iam_role.codepipeline_role.arn
+# resource "aws_codepipeline" "codepipeline" {
+#   name     = "gluejob-pipeline"
+#   role_arn = aws_iam_role.codepipeline_role.arn
 
-  artifact_store {
-    location = aws_s3_bucket.codepipeline_bucket.bucket
-    type     = "S3"
+#   artifact_store {
+#     location = aws_s3_bucket.codepipeline_bucket.bucket
+#     type     = "S3"
 
-    encryption_key {
-      id   = data.aws_kms_alias.s3kmskey.arn
-      type = "KMS"
-    }
-  }
+#     encryption_key {
+#       id   = data.aws_kms_alias.s3kmskey.arn
+#       type = "KMS"
+#     }
+#   }
 
-  stage {
-    name = "Source"
+#   stage {
+#     name = "Source"
 
-    action {
-      name             = "Source"
-      category         = "Source"
-      owner            = "AWS"
-      provider         = "CodeStarSourceConnection"
-      version          = "1"
-      output_artifacts = ["source_output"]
+#     action {
+#       name             = "Source"
+#       category         = "Source"
+#       owner            = "AWS"
+#       provider         = "CodeStarSourceConnection"
+#       version          = "1"
+#       output_artifacts = ["source_output"]
 
-      configuration = {
-        ConnectionArn    = aws_codestarconnections_connection.example.arn
-        FullRepositoryId = "my-organization/example"
-        BranchName       = "main"
-      }
-    }
-  }
+#       configuration = {
+#         ConnectionArn    = aws_codestarconnections_connection.example.arn
+#         FullRepositoryId = "my-organization/example"
+#         BranchName       = "main"
+#       }
+#     }
+#   }
 
-  stage {
-    name = "Build"
+#   stage {
+#     name = "Build"
 
-    action {
-      name             = "Build"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      input_artifacts  = ["source_output"]
-      output_artifacts = ["build_output"]
-      version          = "1"
+#     action {
+#       name             = "Build"
+#       category         = "Build"
+#       owner            = "AWS"
+#       provider         = "CodeBuild"
+#       input_artifacts  = ["source_output"]
+#       output_artifacts = ["build_output"]
+#       version          = "1"
 
-      configuration = {
-        ProjectName = "test"
-      }
-    }
-  }
+#       configuration = {
+#         ProjectName = "test"
+#       }
+#     }
+#   }
 
-}
+# }
 
-resource "aws_codestarconnections_connection" "example" {
-  name          = "example-connection"
-  provider_type = "GitHub"
-}
+# resource "aws_codestarconnections_connection" "example" {
+#   name          = "example-connection"
+#   provider_type = "GitHub"
+# }
 
-resource "aws_s3_bucket" "codepipeline_bucket" {
-  bucket = "test-bucket"
-}
+# resource "aws_s3_bucket" "codepipeline_bucket" {
+#   bucket = "test-bucket"
+# }
 
-resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_pab" {
-  bucket = aws_s3_bucket.codepipeline_bucket.id
+# resource "aws_s3_bucket_public_access_block" "codepipeline_bucket_pab" {
+#   bucket = aws_s3_bucket.codepipeline_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
+# data "aws_iam_policy_document" "assume_role" {
+#   statement {
+#     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["codepipeline.amazonaws.com"]
-    }
+#     principals {
+#       type        = "Service"
+#       identifiers = ["codepipeline.amazonaws.com"]
+#     }
 
-    actions = ["sts:AssumeRole"]
-  }
-}
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
 
-resource "aws_iam_role" "codepipeline_role" {
-  name               = "test-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
+# resource "aws_iam_role" "codepipeline_role" {
+#   name               = "test-role"
+#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+# }
 
-data "aws_iam_policy_document" "codepipeline_policy" {
-  statement {
-    effect = "Allow"
+# data "aws_iam_policy_document" "codepipeline_policy" {
+#   statement {
+#     effect = "Allow"
 
-    actions = [
-      "s3:GetObject",
-      "s3:GetObjectVersion",
-      "s3:GetBucketVersioning",
-      "s3:PutObjectAcl",
-      "s3:PutObject",
-    ]
+#     actions = [
+#       "s3:GetObject",
+#       "s3:GetObjectVersion",
+#       "s3:GetBucketVersioning",
+#       "s3:PutObjectAcl",
+#       "s3:PutObject",
+#     ]
 
-    resources = [
-      aws_s3_bucket.codepipeline_bucket.arn,
-      "${aws_s3_bucket.codepipeline_bucket.arn}/*"
-    ]
-  }
+#     resources = [
+#       aws_s3_bucket.codepipeline_bucket.arn,
+#       "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+#     ]
+#   }
 
-  statement {
-    effect    = "Allow"
-    actions   = ["codestar-connections:UseConnection"]
-    resources = [aws_codestarconnections_connection.example.arn]
-  }
+#   statement {
+#     effect    = "Allow"
+#     actions   = ["codestar-connections:UseConnection"]
+#     resources = [aws_codestarconnections_connection.example.arn]
+#   }
 
-  statement {
-    effect = "Allow"
+#   statement {
+#     effect = "Allow"
 
-    actions = [
-      "codebuild:BatchGetBuilds",
-      "codebuild:StartBuild",
-    ]
+#     actions = [
+#       "codebuild:BatchGetBuilds",
+#       "codebuild:StartBuild",
+#     ]
 
-    resources = ["*"]
-  }
-}
+#     resources = ["*"]
+#   }
+# }
 
-resource "aws_iam_role_policy" "codepipeline_policy" {
-  name   = "codepipeline_policy"
-  role   = aws_iam_role.codepipeline_role.id
-  policy = data.aws_iam_policy_document.codepipeline_policy.json
-}
+# resource "aws_iam_role_policy" "codepipeline_policy" {
+#   name   = "codepipeline_policy"
+#   role   = aws_iam_role.codepipeline_role.id
+#   policy = data.aws_iam_policy_document.codepipeline_policy.json
+# }
 
-data "aws_kms_alias" "s3kmskey" {
-  name = "alias/myKmsKey"
-}
+# data "aws_kms_alias" "s3kmskey" {
+#   name = "alias/myKmsKey"
+# }
